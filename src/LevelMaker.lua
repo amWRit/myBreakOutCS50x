0ae3d4f2ca819c7effd8cf13dc8a94126a732ca5
amWRit <derasrocks@gmail.com>
2020-09-26 19:08:58 +0545

    added color, tier to bricks and different maps

--- src/LevelMaker.lua
+++ src/LevelMaker.lua
@@ -1,12 +1,55 @@
+-- shape of map
+NONE = 1
+SINGLE_PYRAMID = 2
+MULTI_PYRAMID = 3
+
+-- patterns in rows
+SOLID = 1
+ALTERNATE = 2
+SKIP = 3
+NONE = 4
+
 LevelMaker = Class{}
 
 function LevelMaker.createMap(level)
 	local bricks = {}
 	local numRows = math.random(1,5)
 	local numCols = math.random(7,13)
+    numCols = numCols % 2 == 0 and (numCols + 1) or numCols
+
+    local highestTier = math.min(3, math.floor(level/5))
+    local highestColor = math.min(5, level%5+3)
 
 	for y = 1, numRows do
-		for x = 1, numCols do 
+
+        local skipPattern = math.random(1, 2) == 1 and true or false
+        local alternatePattern = math.random(1, 2) == 1 and true or false
+
+        local alternateColor1 = math.random(1, highestColor)
+        local alternateColor2 = math.random(1, highestColor)
+        local alternateTier1 = math.random(0, highestTier)
+        local alternateTier2 = math.random(0, highestTier)
+
+        local skipFlag = math.random(2)  == 1 and true or false
+        local alternateFlag = math.random(2)  == 1 and true or false
+
+        local solidColor = math.random(1, highestColor)
+        local solidTier = math.random(0, highestTier)
+
+
+		for x = 1, numCols do
+            -- if skipping is turned on and we're on a skip iteration...
+            if skipPattern and skipFlag then
+                -- turn skipping off for the next iteration
+                skipFlag = not skipFlag
+
+                -- Lua doesn't have a continue statement, so this is the workaround
+                goto continue
+            else
+                -- flip the flag to true on an iteration we don't use it
+                skipFlag = not skipFlag
+            end
+
             b = Brick(
                 -- x-coordinate
                 (x-1)                   -- decrement x by 1 because tables are 1-indexed, coords are 0
@@ -18,8 +61,32 @@ function LevelMaker.createMap(level)
                 y * 16                  -- just use y * 16, since we need top padding anyway
             ) 
 
+            -- if we're alternating, figure out which color/tier we're on
+            if alternatePattern and alternateFlag then
+                b.color = alternateColor1
+                b.tier = alternateTier1
+                alternateFlag = not alternateFlag
+            else
+                b.color = alternateColor2
+                b.tier = alternateTier2
+                alternateFlag = not alternateFlag
+            end
+
+            -- if not alternating and we made it here, use the solid color/tier
+            if not alternatePattern then
+                b.color = solidColor
+                b.tier = solidTier
+            end 
+
             table.insert(bricks, b)
+            -- Lua's version of the 'continue' statement
+            ::continue::
 		end
 	end
-	return bricks
+    -- in the event we didn't generate any bricks, try again
+    if #bricks == 0 then
+        return self.createMap(level)
+    else
+        return bricks
+    end
 end
